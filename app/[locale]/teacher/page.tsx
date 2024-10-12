321e908c82b9119f3c41d6fbe1ceb2632310d0ae
Yassmin911 <ayassmin1998@gmail.com>
2024-10-12 14:32:18 +0300

    UI components

--- app/[locale]/teacher/page.tsx
+++ app/[locale]/teacher/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeIntro from '../home/sections/intro';
-import Advantages from './sections/advantages';
+import Advantages from './sections/how-to-start';
+import HowToStart from './sections/how-to-start';
 
 const TeacherPage: React.FC = () => {
   return (
     <div>
       <HomeIntro bgColor='#422e87' isBgImage={false} />
+      <HowToStart />
     </div>
   );
 };
